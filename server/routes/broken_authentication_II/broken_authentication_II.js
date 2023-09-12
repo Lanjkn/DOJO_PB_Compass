124b82242e4c2ff0253db0b0b5d5a2708dc33376
Vinicius <105178428+ViniStark@users.noreply.github.com>
2023-09-12 12:35:28 -0300

    Add files via upload

--- server/routes/broken_authentication_II/broken_authentication_II.js
+++ server/routes/broken_authentication_II/broken_authentication_II.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 const { errHandling } = require('../../utils/utils');
 const cookieParser = require('cookie-parser');
 const { getUserById, updateUsername } = require('../../service/service');
-const jwt = require('jsonwebtoken');
+
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
 
 router.use(cookieParser());
 
@@ -11,16 +15,14 @@ const renderData = {};
 router.get(
 	'/broken_authentication_II',
 	errHandling(async (req, res) => {
-		const { jwt_token } = req.cookies;
-		const usuarioNaoAutenticado = jwt_token == undefined;
+		const { user_id } = req.cookies;
+		const usuarioNaoAutenticado = user_id == undefined;
 
 		if (usuarioNaoAutenticado) {
 			res.render('user-not-authenticated');
 		} else {
-			const { user_id } = jwt.verify(jwt_token, process.env.TOKEN_KEY);
 			const { rows } = await getUserById(user_id);
 			renderData.username = rows[0].username;
-			renderData.user_id = user_id;
 			res.render('broken_authentication_II', renderData);
 		}
 	})
@@ -31,15 +33,14 @@ router.get(
 	errHandling(async (req, res) => {
 		//CRIA A VARIAVEI COM BASE NO QUE VEIO NA URL
 		const { novo_username } = req.query;
+		const nomeClean = DOMPurify.sanitize(novo_username);
 		//CRIA A VARIAVEI COM BASE NO QUE ESTA NOS COOKIES
-		const { jwt_token } = req.cookies;
+		const { user_id } = req.cookies;
 		//BUSCA NO BANCO DE DADOS SE O USUARIO EXISTE
-		const { user_id } = jwt.verify(jwt_token, process.env.TOKEN_KEY);
-
 		const { rows } = await getUserById(user_id);
 		const userExiste = rows.length == 1;
 		if (userExiste) {
-			const { rows } = await updateUsername(novo_username, user_id);
+			const { rows } = await updateUsername(nomeClean, user_id);
 			renderData.username = rows[0].username;
 			res.render('broken_authentication_II', renderData);
 		} else {
