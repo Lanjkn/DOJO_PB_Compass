13454973b81f69164ee53743554452217e5a70dc
Vinicius <105178428+ViniStark@users.noreply.github.com>
2023-09-12 12:33:55 -0300

    Add files via upload

--- server/routes/broken_authentication/broken_authentication.js
+++ server/routes/broken_authentication/broken_authentication.js
@@ -2,8 +2,11 @@ const router = require('express').Router();
 const { errHandling } = require('../../utils/utils');
 const cookieParser = require('cookie-parser');
 const { updateUsername, getUserById } = require('../../service/service');
-const { json } = require('express');
-const jwt = require('jsonwebtoken');
+
+const createDOMPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const window = new JSDOM('').window;
+const DOMPurify = createDOMPurify(window);
 
 router.use(cookieParser());
 
@@ -12,13 +15,12 @@ const renderData = {};
 router.get(
 	'/broken_authentication',
 	errHandling(async (req, res) => {
-		const { jwt_token } = req.cookies;
-		const usuarioNaoAutenticado = jwt_token == undefined;
+		const { user_id } = req.cookies;
+		const usuarioNaoAutenticado = user_id == undefined;
 
 		if (usuarioNaoAutenticado) {
 			res.render('user-not-authenticated');
 		} else {
-			const { user_id } = jwt.verify(jwt_token, process.env.TOKEN_KEY);
 			const { rows } = await getUserById(user_id);
 			renderData.username = rows[0].username;
 			renderData.user_id = user_id;
@@ -32,24 +34,13 @@ router.get(
 	errHandling(async (req, res) => {
 		//CRIA A VARIAVEL COM BASE NO QUE VEIO NA URL
 		const { id: user_id, novo_username } = req.query;
+		const nomeClean = DOMPurify.sanitize(novo_username);
 		renderData.user_id = user_id;
 		//BUSCA NO BANCO DE DADOS SE O USUARIO EXISTE
-		jwt_token = req.cookies.jwt_token;
-
-		const { user_id: user_id_token } = jwt.verify(
-			jwt_token,
-			process.env.TOKEN_KEY
-		);
-		const usuarioAutenticado = user_id_token == user_id;
-		if (!usuarioAutenticado) {
-			renderData.username = 'User_id_autentication_failed';
-			res.render('broken_authentication', renderData);
-			return;
-		}
 		const { rows } = await getUserById(user_id);
 		const userExiste = rows.length == 1;
 		if (userExiste) {
-			const { rows } = await updateUsername(novo_username, user_id);
+			const { rows } = await updateUsername(nomeClean, user_id);
 			renderData.username = rows[0].username;
 			res.render('broken_authentication', renderData);
 		} else {
